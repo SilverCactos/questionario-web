4732315289cb146a7a9a02921790d90490feb7ff
SilverCactos <srcactos@gmail.com>
2026-07-28 20:22:44 -0300

    resumo1 1.0

--- python.js
+++ python.js
@@ -1,13 +1,24 @@
 function voltar() {
     document.getElementById("botoes").style.display = "flex";
     document.getElementById("botoes2").style.display = "none";
-    document.getElementById("textoresumo").style.display = "none";
+    document.getElementById("div1").style.display = "none";
 }
 
 function voltar1() {
-    document.getElementById("botoes").style.display = "flex";
-    document.getElementById("botoes2").style.display = "none";
-    document.getElementById("textoresumo").style.display = "none";
+    document.getElementById("botoes").style.display = "none";
+    document.getElementById("botoes2").style.display = "flex";
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("texto1").innerHTML = "Selecione seu caminho:";
+}
+
+function voltar2() {
+    document.getElementById("botoes2").style.display = "flex";
+    document.getElementById("div2").style.display = "none";
+}
+
+function voltar3() {
+    document.getElementById("botoes2").style.display = "flex";
+    document.getElementById("div3").style.display = "none";
 }
 
 function botaoR() {
@@ -17,6 +28,18 @@ function botaoR() {
 
 function botaoR1() {
     document.getElementById("botoes2").style.display = "none";
-    document.getElementById("textoresumo").style.display = "flex";
+    document.getElementById("div1").style.display = "flex";
+    document.getElementById("texto1").innerHTML = "Básico do Pré-Python";
 
+}
+
+function botaoR2() {
+    document.getElementById("botoes2").style.display = "none";
+    document.getElementById("div2").style.display = "flex";
+
+}
+
+function botaoR3() {
+    document.getElementById("botoes2").style.display = "none";
+    document.getElementById("div3").style.display = "flex";
 }
